00bfc0c0608703e2409ca8609c39f243ae86a443
fcorreas <fcorreas23@gmail.com>
2021-06-03 15:19:56 -0400

    augustus

--- src/views/Augustus.vue
+++ src/views/Augustus.vue
@@ -8,7 +8,7 @@
       <v-card-text>
         <p>AUGUSTUS is a software tool for gene prediction in eukaryotes based on a Generalized Hidden Markov Model, a probabilistic model of a sequence and its gene structure. Like most existing gene finders, the first version of AUGUSTUS returned one transcript per predicted gene and ignored the phenomenon of alternative splicing. </p>
         <v-alert  border="top" colored-border type="info" elevation="2">
-           En desarrollo de interfaz web
+           En desarrollo de interfaz web y backend
         </v-alert>
         <iframe style="display:block; width:100%; height:100vh;" src="http://bioinf.uni-greifswald.de/webaugustus/"></iframe>
 
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+import { createLogger } from 'vuex'
 // @ is an alias to /src
 
 export default {
